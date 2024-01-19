6e93d04cf94487b917beee98d34b59190e4e9c73
Kalyanchenumalla <kalyan7.aug@gmail.com>
2024-01-19 22:07:41 +0530

    END13A - Angular and Dotnet Pagination

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -23,9 +23,10 @@ public class UserRepository : IUserRepository
         .SingleOrDefaultAsync();
     }
 
-    public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+    public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
     {
-        return await _context.Users.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).ToListAsync();
+        var query = _context.Users.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).AsNoTracking();
+        return await PagedList<MemberDto>.CreateAsync(query, userParams.pageNumber, userParams.PageSize);
     }
 
     public async Task<AppUser> GetUserByIdAsync(int id)
